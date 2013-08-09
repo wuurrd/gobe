a79ff04a869482ee78bbc1a5af36112d4d8c4983
David Buchmann <davbuchm@cisco.com>
2013-08-09 22:48:29 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='gobe',
+      description="A web proxy written using tulip",
+      url='http://github.com/wuurrd/gobe.git',
+      packages=['gobe'],
+      )
